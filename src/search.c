e3904f2b79aec349a9a44e457212b0db616145a1
Josh Hayden <blindcollective@protonmail.com>
2025-01-28 23:36:58 -0500

    Removed inefficient helper function to instead use a pointer to the expression associated with the naming convention passed.

--- src/search.c
+++ src/search.c
@@ -27,50 +27,36 @@ void search_directory(const char *dir_path, const char *arg_naming_convention, i
 	/* dir_path is known to be valid at this point. */
 	DIR *dir = opendir(dir_path);	
 	struct dirent *dp;
+	
+	char *search_expression;
+	if (strcmp("camelcase", arg_naming_convention) == 0)		search_expression = EXPR_CAMELCASE;
+	else if (strcmp("snakecase", arg_naming_convention) == 0)	search_expression = EXPR_SNAKECASE;
+	else if (strcmp("kebabcase", arg_naming_convention) == 0)	search_expression = EXPR_KEBABCASE;
+
 	while ((dp = readdir(dir)) != NULL) {
 		/* Skip current and parent entries. */
 		if (strcmp(dp->d_name, ".") == 0 || strcmp(dp->d_name, "..") == 0)
 			continue;
-		/* 
-		 * Helper function to naming_match_regex(). Will decide what pattern to pass into
-		 * naming_match_regex(), pass it in, then increment *matches if it returns true.
-		 */
-		void helper_naming_match_regex()
-		{
-			switch (arg_naming_convention[0]) {
-			case 'c':
-				if (naming_match_regex(EXPR_CAMELCASE, dp->d_name)) {
-					printf("%s\n", dp->d_name); 
-					(*matches)++;
-				}
-				break;
-			case 's':
-				if (naming_match_regex(EXPR_SNAKECASE, dp->d_name)) {
-					printf("%s\n", dp->d_name); 
-					(*matches)++;
-				}
-				break;
-			case 'k':
-				if (naming_match_regex(EXPR_KEBABCASE, dp->d_name)) {
-					printf("%s\n", dp->d_name); 
-					(*matches)++;
-				}
-				break;
-			}
-		}
-		
-				
+
 		char full_path[PATH_MAX];
 		snprintf(full_path, sizeof(full_path), "%s/%s", dir_path, dp->d_name);
+
 		struct stat statbuf;
 		if (stat(full_path, &statbuf) == 0) {
 			if (S_ISDIR(statbuf.st_mode)) {
 				/* If it's a directory, check it's name then recurse. */
-				helper_naming_match_regex(); 
+				if (naming_match_regex(search_expression, dp->d_name)) {
+					printf("%s\n", dp->d_name); 
+					(*matches)++;
+				} 
 				/* Recurse each subdirectory. */
 				search_directory(full_path, arg_naming_convention, matches);
-			} else if (S_ISREG(statbuf.st_mode)) 
-				helper_naming_match_regex(); 	
+			} else if (S_ISREG(statbuf.st_mode)) { 
+				if (naming_match_regex(search_expression, dp->d_name)) {
+					printf("%s\n", dp->d_name); 
+					(*matches)++;
+				} 
+			}	
 		}
 	}
 	closedir(dir);
