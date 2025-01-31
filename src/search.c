8d406d61e66a9f3d9d397c5920e89bb8e0dbe1e8
Josh Hayden <blindcollective@protonmail.com>
2025-01-30 20:42:34 -0500

    Fixed output to show only filename if no option is set, and full path if -f is set.

--- src/search.c
+++ src/search.c
@@ -19,7 +19,7 @@
 #define EXPR_SNAKECASE "^[a-z]+(_[a-z]+)+$"
 #define EXPR_KEBABCASE "^[a-z]+(-[a-z]+)+$"
 
-void print_formatted_match_output(struct dirent *dp, char full_path[PATH_MAX])
+void print_search_results(struct dirent *dp, char full_path[PATH_MAX])
 {
 	if (full_path_flag == 1)
 		printf("%s%s\n", full_path, dp->d_name);
@@ -59,16 +59,14 @@ void search_directory(const char *dir_path, const char *arg_naming_convention, i
 			if (S_ISDIR(statbuf.st_mode)) {
 				/* If it's a directory, check it's name then recurse. */
 				if (naming_match_regex(search_expression, dp->d_name)) {
-					printf("%s\n", full_path);
-					//print_formatted_match_output(dp, full_path);	
+					print_search_results(dp, full_path);	
 					(*matches)++;
 				} 
 				/* Recurse each subdirectory. */
 				search_directory(full_path, arg_naming_convention, matches);
 			} else if (S_ISREG(statbuf.st_mode)) { 
 				if (naming_match_regex(search_expression, dp->d_name)) {
-					printf("%s\n", full_path);
-					//print_formatted_match_output(dp, full_path);	
+					print_search_results(dp, full_path);	
 					(*matches)++;
 				} 
 			}	
